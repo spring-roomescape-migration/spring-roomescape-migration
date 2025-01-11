397d07027eb39194fdb5c25bd5297e63a51820de
박민욱 <88137420+johnprk@users.noreply.github.com>
2025-01-11 22:14:30 +0900

    [SCRUM-17] 프론트 변수명 수정 (#17)

--- src/main/resources/static/js/user-reservation.js
+++ src/main/resources/static/js/user-reservation.js
@@ -184,13 +184,10 @@ function onWaitButtonClick() {
     if (selectedDate && selectedThemeId && selectedTimeId) {
         const reservationData = {
             date: selectedDate,
-            theme: selectedThemeId,
-            time: selectedTimeId
+            themeId: selectedThemeId,
+            timeId: selectedTimeId
         };
 
-        /*
-        TODO: [3단계] 예약 대기 생성 요청 API 호출
-         */
         fetch('reservations/wait', {
             method: 'POST',
             headers: {
