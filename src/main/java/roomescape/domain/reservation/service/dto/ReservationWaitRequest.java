4751ff30855aea7645f28890b1cbee060fb634ad
JohnPrk <uo3641493@gmail.com>
2025-01-11 22:13:07 +0900

    fix : @JsonProperty 변수명 수정

--- src/main/java/roomescape/domain/reservation/service/dto/ReservationWaitRequest.java
+++ src/main/java/roomescape/domain/reservation/service/dto/ReservationWaitRequest.java
@@ -12,8 +12,8 @@ public class ReservationWaitRequest {
     @JsonCreator
     public ReservationWaitRequest(
             @JsonProperty("date") String date,
-            @JsonProperty("timeId") Long timeId,
-            @JsonProperty("themeId") Long themeId) {
+            @JsonProperty("time") Long timeId,
+            @JsonProperty("theme") Long themeId) {
         this.date = date;
         this.timeId = timeId;
         this.themeId = themeId;
